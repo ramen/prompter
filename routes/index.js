f80ff31cd59107cb8bd6360fdd3965ab8d84ae88
Dave Benjamin <dave@teamaud.io>
2025-03-02 18:54:48 -0700

    Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fly.io' });
+  res.render('index', { title: 'Prompter' });
 });
 
 module.exports = router;
